a402132ef4042f09d984553733b338aa41af116e
saraakaram <sara.karam01@lau.edu>
2025-05-04 00:12:19 +0300

    Use ModelService class and introduce an endpoint to add more file types

--- src/controllers/file_controller.py
+++ src/controllers/file_controller.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, request, jsonify
 from src.services.file_service import get_file_extension, is_valid, process_file
-from src.services.classification_service import classify_file
+from src.services.model_service import ModelService
 
 """
 This controller handles file-related endpoints.
 """
 file_bp = Blueprint('file', __name__)
+model_service = ModelService.get_instance()
 
 @file_bp.route('/classify_file', methods=['POST'])
 def classify_file_route():
@@ -25,7 +26,31 @@ def classify_file_route():
     if not is_valid(file_extension):
         return jsonify({"error": f"File type not allowed"}), 400
 
-    # TODO: Modify this after implementing new classification method
     file_text = process_file(file, file_extension)
-    file_class = classify_file(file)
+    file_class = model_service.classify_file(file_text)
     return jsonify({"file_class": file_class}), 200
+
+@file_bp.route("/add_class", methods=["POST"])
+def add_file_type():
+    """
+    Endpoint to add new file types (new classes)
+    """
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part in the request"}), 400
+
+    sample_file = request.files['file']
+    file_type = request.form.get('file_type', '').strip().lower()
+
+    if not file_type or sample_file.filename == '':
+        return jsonify({"error": "File type and file are required."}), 400
+
+    file_extension = get_file_extension(sample_file.filename)
+    if not is_valid(file_extension):
+        return jsonify({"error": "File type not allowed"}), 400
+
+    try:
+        file_text = process_file(sample_file, file_extension)
+        model_service.embed_file(file_type, file_text)
+        return jsonify({"message": f"File type '{file_type}' registered successfully."}), 200
+    except Exception as e:
+        return jsonify({"error": f"Internal server error: {str(e)}"}), 500
